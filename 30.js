e8fcf01f36dfe20afd733aaa270e840e6ba0587e
KimDoHyeon <ghkdtprhf5@naver.com>
2024-05-24 23:09:01 +0900

    feat : 백준 10610번 30 구현

---
+++ 30.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input;
+rl.on('line', function (line) {
+  input = line.split('').map(Number);
+  rl.close();
+}).on('close', function () {
+  console.log(solution(input));
+});
+
+function solution(num) {
+  if (!num.includes(0)) {
+    return -1;
+  }
+  if (num.reduce((acc, cur) => acc + cur, 0) % 3 !== 0) {
+    return -1;
+  }
+
+  num = num.sort((a, b) => b - a);
+
+  return num.join('');
+}
+
+/*
+a=BigInt((''+require('fs').readFileSync(0)).split``.sort((a,b)=>b-a).join``)
+console.log(a%30n?-1:''+a)
+*/
